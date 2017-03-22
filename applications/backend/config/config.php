339bc1b378113058a3514cd50c04581af9028125
Fathoni <m.fathoni@mail.com>
2017-03-22 13:16:41 +0700

    Ubah config backend

--- applications/backend/config/config.php
+++ applications/backend/config/config.php
@@ -378,7 +378,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'simpkmi-be';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = APPPATH . 'sessions';
 $config['sess_match_ip'] = FALSE;
