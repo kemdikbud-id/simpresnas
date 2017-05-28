535625ad4e02b7e5036d2ea8b9cd4d2f32ba58fc
Fathoni <m.fathoni@mail.com>
2017-05-28 12:59:33 +0700

    tambahan pengecekan file config email.php

--- applications/admin/config/email.php
+++ applications/admin/config/email.php
@@ -3,5 +3,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if (file_exists('../shared/config/email.php'))
 	include_once APPPATH . '../shared/config/email.php';
-else
-	show_error ("Email configuration not found", 500, "Configuration error");
