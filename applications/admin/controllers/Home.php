b8c87d06e1eed2ec12134a1629f395cf08cd500d
Fathoni <m.fathoni@mail.com>
2019-04-16 16:22:50 +0700

    Tambah fungsi test email

--- applications/admin/controllers/Home.php
+++ applications/admin/controllers/Home.php
@@ -2,6 +2,7 @@
 
 /**
  * @author Fathoni <m.fathoni@mail.com>
+ * @property CI_Email $email
  */
 class Home extends Admin_Controller
 {
@@ -15,5 +16,25 @@ class Home extends Admin_Controller
 	public function index()
 	{
 		$this->smarty->display();
+	}
+	
+	public function test_email()
+	{
+		$this->load->library('email');
+		
+		$this->email->from('no-reply@ristekdikti.go.id', 'SIM-PKMI');
+		$this->email->to('mokhammad.fathoni.rokhman@gmail.com');
+		$this->email->cc('m.fathoni@mail.com');
+		$this->email->subject('Jajal Email');
+		$this->email->message('Ini adalah email jajal body');
+		
+		if ($this->email->send())
+		{
+			echo "Berhasil";
+		}
+		else
+		{
+			show_error($this->email->print_debugger());
+		}
 	}
 }
